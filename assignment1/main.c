fa5d3ac4797da5781b0e20dce2ed813d212d261e
brycebeagle <bryce.beagle@gmail.com>
2018-01-22 14:44:02 -0700

    Program struct is now created properly

--- assignment1/main.c
+++ assignment1/main.c
@@ -27,7 +27,10 @@ typedef struct Operation{
 typedef struct Thread {
 
     ThreadType threadType;
-    long period;  // long was chosen arbitrarily
+    long priority;  // long was chosen arbitrarily
+    long period;    // long was chosen arbitrarily
+    long trigger;   // long was chosen arbitrarily
+
     Operation *operations;
 
 } Thread;
@@ -51,24 +54,27 @@ void busyLoop(int iterations) {
     }
 }
 
-void periodic(void *structure) {
+void *periodic(void *ptr) {
+
+    Thread *thread = (Thread *)ptr;
 
     // Local variables
     // Init
 
     // Wait for activation
 
-//    while (1) {
-//
-//        busyLoop(0);
-//        // Wait for period
-//
-//    }
+    while (1) {
+
+        busyLoop(0);
+        // Wait for period
+
+    }
 
 }
 
+void *aperiodic(void *ptr) {
 
-void aperiodic(void *structure) {
+    Thread *thread = (Thread *)ptr;
 
     // Local variables
     // Init
@@ -77,8 +83,8 @@ void aperiodic(void *structure) {
 
     // Wait for event
 
-//    while(1) {
-//
+    while(1) {
+
 //        if (lock) {
 //            pthread_mutex_lock(mutexes[lockNumber]);
 //        }
@@ -86,19 +92,21 @@ void aperiodic(void *structure) {
 //        if (unlock) {
 //            pthread_mutex_unlock(mutexes[lockNumber]);
 //        }
-//
-//        busyLoop(0);
-//        // Wait of event
-//
-//    }
+
+        busyLoop(0);
+        // Wait of event
+
+    }
 
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
-//MAIN PROGRAM CODE
+// MAIN PROGRAM CODE
 
 ProgramInfo parseFile(char *filename) {
+
+    // TODO: Too long. segment into multiple functions
 
     ProgramInfo program;
     FILE* file = fopen(filename, "r");  // TODO: Default?
@@ -108,8 +116,8 @@ ProgramInfo parseFile(char *filename) {
 
     // First line designating number of threads and runtime
     fgets(line, sizeof(line), file);
-    program.numThreads = strtol(line, &word_end, 10);
-    program.duration   = strtol(line, &word_end, 10);
+    program.numThreads = strtol(line,     &word_end, 10);
+    program.duration   = strtol(word_end, &word_end, 10);
     program.threads    = malloc(program.numThreads * sizeof(Thread));
 
     // Thread declaration
@@ -118,43 +126,64 @@ ProgramInfo parseFile(char *filename) {
 
     for (int i = 0; i < program.numThreads; i++) {
 
-        Thread thread = program.threads[i];
+        Thread *thread = &program.threads[i];
 
         // Get line declaring thread
         fgets(line, sizeof(line), file);
 
-        Operation *root;
-        Operation **operation = &root;
-
-        thread.operations = root;
-
+        // Thread type
         token = strtok_r(line, " ", &state);
+        switch(token[0]) {
+            case 'P': thread->threadType = PERIODIC; break;
+            case 'A': thread->threadType = APERIODIC; break;
+            default: break;
+        }
+
+        // Thread priority
+        token = strtok_r(NULL, " ", &state);
+        thread->priority = strtol(token, NULL, 10);
+
+        // Aperiodic Trigger
+        if (thread->threadType == APERIODIC) {
+            token = strtok_r(NULL, " ", &state);
+            thread->trigger = strtol(token, NULL, 10);
+        }
+
+        // Thread period
+        token = strtok_r(NULL, " ", &state);
+        thread->period = strtol(token, NULL, 10);
+
+        // Thread operations
+        Operation *root = NULL;
+        Operation *tail = NULL;
+
+        token = strtok_r(NULL, " ", &state);
         while(token) {
 
-            *operation = malloc(sizeof(Operation));
+            Operation *operation = malloc(sizeof(Operation));
 
             switch(token[0]) {
-                case 'P':
-                    thread.threadType = PERIODIC;
-                    break;
-                case 'A':
-                    thread.threadType = APERIODIC;
-                    break;
                 case 'L':
-                    (*operation)->operation = LOCK;
-                    (*operation)->value = strtol(token + 1, NULL, 10);
-                    operation = &(*operation)->nextOp;
+                    operation->operation = LOCK;
+                    operation->value = strtol(token + 1, NULL, 10);
                     break;
                 case 'U':
-                    (*operation)->operation = UNLOCK;
-                    (*operation)->value = strtol(token + 1, NULL, 10);
-                    operation = &(*operation)->nextOp;
+                    operation->operation = UNLOCK;
+                    operation->value = strtol(token + 1, NULL, 10);
                     break;
                 default:
-                    (*operation)->operation = BUSY_LOOP;
-                    (*operation)->value = strtol(token, NULL, 10);
-                    operation = &(*operation)->nextOp;
+                    operation->operation = BUSY_LOOP;
+                    operation->value = strtol(token, NULL, 10);
                     break;
+            }
+
+            if (tail == NULL) {
+                tail = operation;
+                root = operation;
+            }
+            else {
+                tail->nextOp = operation;
+                tail = operation;
             }
 
             // Get next token
@@ -162,20 +191,30 @@ ProgramInfo parseFile(char *filename) {
         }
 
         // Terminate last node
-        *operation = NULL;
+        if (tail != NULL) {
+            tail->nextOp = NULL;
+        }
+
+        thread->operations = root;
+
     }
+
+    return program;
+
 }
 
 void main(int argc, char* argv[]) {
 
-    ProgramInfo threadStructure = parseFile(argv[1]);
+    ProgramInfo program = parseFile(argv[1]);
 
-    pthread_t threads[threadStructure.numThreads];
+    pthread_t threads[program.numThreads];  // TODO: Use later
 
-    // Parse file
-    // Alloc space for each thread
+    for (int i=0; i < program.numThreads; i++) {
 
-
-
-
+        switch (program.threads[0].threadType) {
+            case  PERIODIC:  pthread_create(&threads[i], NULL, periodic, &program.threads[0]); break;
+            case APERIODIC: aperiodic(&program.threads[0]); break;
+            default: break;
+        }
+    }
 }
