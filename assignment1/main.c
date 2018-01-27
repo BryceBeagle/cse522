527ebba5fd24c1bce1d5c01e9213d82521f35358
brycebeagle <bryce.beagle@gmail.com>
2018-01-27 15:40:51 -0700

    Aperiodic thread and other improvements. Mouse input is definitely broken as only left clicks work

--- assignment1/main.c
+++ assignment1/main.c
@@ -2,30 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <pthread.h>
+
 #include <fcntl.h>
 #include <unistd.h>
-#include <time.h>
+
 #include <linux/input.h>
 
 ////////////////////////////////////////////////////////////////////////////////
 // Global variables
-pthread_mutex_t mutexes[10] = {PTHREAD_MUTEX_ERRORCHECK};
-pthread_mutex_t activate_mut = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t activate_left = PTHREAD_COND_INITIALIZER;
-pthread_cond_t activate_right = PTHREAD_COND_INITIALIZER;
+pthread_barrier_t thread_sync;
 
+pthread_mutex_t mutexes[10] = {PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP};
+pthread_mutex_t activate_mut = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t activate_cond = PTHREAD_COND_INITIALIZER;
+
+pthread_mutex_t event_mut = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
+pthread_cond_t event_cond[2] = {PTHREAD_COND_INITIALIZER};
+
+int initialized = 0;
 
 ////////////////////////////////////////////////////////////////////////////////
 // DATA STRUCTURES
 
 typedef enum {LOCK, UNLOCK, BUSY_LOOP} OperationType;
 typedef enum {PERIODIC, APERIODIC} ThreadType;
+enum {LEFT, RIGHT};
 
 typedef struct Operation{
 
-    OperationType operation;
-    long value;  // Mutex number or number of iterations. long was chosen arbitrarily
+    OperationType     operation;
+    unsigned long     value;  // Mutex number or number of iterations. long was chosen arbitrarily
 
     struct Operation *nextOp;
 
@@ -33,26 +41,28 @@ typedef struct Operation{
 
 typedef struct Thread {
 
-    ThreadType threadType;
-    long priority;  // long was chosen arbitrarily
-    long period;    // long was chosen arbitrarily
-    long trigger;   // long was chosen arbitrarily
+    ThreadType    thread_type;
+    unsigned int  priority;
+    unsigned long period;    // long was chosen arbitrarily
+    unsigned long event;     // long was chosen arbitrarily
 
-    Operation *operations;
+    Operation    *operations;
 
 } Thread;
 
 typedef struct {
 
-    long numThreads;  // long was chosen arbitrarily
-    long duration;    // long was chosen arbitrarily
-    Thread *threads;
+    unsigned int  numThreads;
+    unsigned long duration;    // long was chosen arbitrarily
+    Thread       *threads;
 
 } ProgramInfo;
 
-void busyLoop(int iterations);
+void busyLoop(long iterations);
 void next_operation(Operation **ptr);
-int msleep(struct timespec start, int msec);
+void do_operation(Operation **operation);
+int msleep(struct timespec start, long msec);
+
 void *periodic(void *ptr);
 void *aperiodic(void *ptr);
 ProgramInfo parseFile(char *filename);
@@ -60,6 +70,7 @@ void *mouse_reader(void *filename);
 
 ////////////////////////////////////////////////////////////////////////////////
 // THREADS
+
 void *mouse_reader(void *filename) {
 
     int fd;
@@ -73,21 +84,25 @@ void *mouse_reader(void *filename) {
 
     while(read(fd, &ie, sizeof(struct input_event))){
         unsigned char *ptr = (unsigned char*)&ie;
-        mouse_left = ptr[0] & (unsigned char)0x1;
-        mouse_right = (ptr[0] & (unsigned char)0x2);
+        mouse_left  = ptr[0] & (unsigned char)0x1;
+        mouse_right = ptr[0] & (unsigned char)0x2;
         if(mouse_left){
             fprintf(stderr, "LEFT\n");
-            pthread_cond_broadcast(&activate_left);
+            pthread_mutex_lock(&event_mut);
+            pthread_cond_broadcast(&event_cond[LEFT]);
+            pthread_mutex_unlock(&event_mut);
         }
         if(mouse_right){
             fprintf(stderr, "RIGHT\n");
-            pthread_cond_broadcast(&activate_right);
+            pthread_mutex_lock(&event_mut);
+            pthread_cond_broadcast(&event_cond[RIGHT]);
+            pthread_mutex_unlock(&event_mut);
         }
     }
     return NULL;
 }
 
-void busyLoop(int iterations) {
+void busyLoop(long iterations) {
     for (int i=0, j=0; i < (iterations); i++) {
         j += i;
     }
@@ -97,10 +112,32 @@ void next_operation(Operation **ptr) {
     if(ptr != NULL && *ptr != NULL) *ptr = (*ptr)->nextOp;
 }
 
-int msleep(struct timespec start, int msec) {
+void do_operation(Operation **operation) {
 
-        start.tv_sec += (long)msec / 1000;
-        start.tv_nsec += ((long)msec%1000) * 1000000L;
+    switch((*operation)->operation) {
+        case LOCK      :
+            pthread_mutex_lock(&mutexes[(*operation)->value]);
+            fprintf(stdout, "%lu :: LOCK %ld\n", pthread_self(), (*operation)->value);
+            break;
+        case UNLOCK    :
+            pthread_mutex_unlock(&mutexes[(*operation)->value]);
+            fprintf(stdout, "%lu :: UNLOCK %ld\n", pthread_self(), (*operation)->value);
+            break;
+        case BUSY_LOOP :
+            busyLoop((*operation)->value);
+
+            struct timespec t;
+            clock_gettime(CLOCK_REALTIME, &t);
+            fprintf(stdout, "%lu :: UNLOCK %ld\n", pthread_self(), (*operation)->value);
+            break;
+    }
+
+}
+
+int msleep(struct timespec start, long msec) {
+
+        start.tv_sec += msec / 1000;
+        start.tv_nsec += (msec%1000) * 1000000L;
 
         if(start.tv_nsec > 999999999){
                 start.tv_nsec -= 1000000000;
@@ -113,43 +150,32 @@ int msleep(struct timespec start, int msec) {
 void *periodic(void *ptr) {
 
     Thread *thread = (Thread *)ptr;
-
-    // Local variables
-    Operation *current_operation = thread->operations;
-
-    // Init
+    struct timespec start_time;
 
     // Wait for activation
+    pthread_barrier_wait(&thread_sync);  // Sync all threads
     pthread_mutex_lock(&activate_mut);
+    while (!initialized) {
+        pthread_cond_wait(&activate_cond, &activate_mut);
+    }
     pthread_mutex_unlock(&activate_mut);
 
     while (1) {
-        struct timespec start_time;
+
+        Operation *current_operation = thread->operations;
+
+        // Get current time
         clock_gettime(CLOCK_REALTIME, &start_time);
 
-        switch(current_operation->operation) {
-            case LOCK      :
-                pthread_mutex_lock(&mutexes[current_operation->value]);
-                fprintf(stderr, "%lu :: LOCK %ld\n", pthread_self(), current_operation->value);
-                break;
-            case UNLOCK    :
-                pthread_mutex_unlock(&mutexes[current_operation->value]);
-                fprintf(stderr, "%lu :: UNLOCK %ld\n", pthread_self(), current_operation->value);
-                break;
-            case BUSY_LOOP :
-                busyLoop(current_operation->value);
-                fprintf(stderr, "%lu :: BUSY_LOOP %ld\n", pthread_self(), current_operation->value);
-                break;
-        }
+        while (current_operation != NULL) {
 
-        next_operation(&current_operation); //advance
-        if(current_operation == NULL){ // loop back around
-            current_operation = thread->operations;
-        }
+            do_operation(&current_operation);
+            next_operation(&current_operation); //advance
 
-        // Wait for period
-        if(msleep(start_time, thread->period) == -1){
-            break;
+            // If thread has gone over period duration, finish current execution and restart it
+            if (msleep(start_time, thread->period) == -1) {
+                break;
+            }
         }
     }
 
@@ -160,28 +186,32 @@ void *aperiodic(void *ptr) {
 
     Thread *thread = (Thread *)ptr;
 
-    // Local variables
-    // Init
-
     // Wait for activation
-
-    // Wait for event
+    pthread_barrier_wait(&thread_sync);  // Sync all threads
+    pthread_mutex_lock(&activate_mut);
+    while (!initialized) {
+        pthread_cond_wait(&activate_cond, &activate_mut);
+    }
+    pthread_mutex_unlock(&activate_mut);
 
     while(1) {
 
-       // if (lock) {
-       //     pthread_mutex_lock(mutexes[lockNumber]);
-       // }
+        Operation *current_operation = thread->operations;
 
-       // if (unlock) {
-       //     pthread_mutex_unlock(mutexes[lockNumber]);
-       // }
+        while (current_operation != NULL) {
 
-        busyLoop(0);
-        // Wait of event
+            // Wait for next event
+            pthread_mutex_lock(&event_mut);
+            pthread_cond_wait(&event_cond[thread->event], &event_mut);
+            pthread_mutex_unlock(&event_mut);
 
+            fprintf(stderr, "Click detected\n");
+
+            do_operation(&current_operation);
+            next_operation(&current_operation);
+        }
     }
-
+    return NULL;
 }
 
 
@@ -200,15 +230,15 @@ ProgramInfo parseFile(char *filename) {
 
     // First line designating number of threads and runtime
     fgets(line, sizeof(line), file);
-    program.numThreads = strtol(line,     &word_end, 10);
-    program.duration   = strtol(word_end, &word_end, 10);
+    program.numThreads = (int) strtoul(line, &word_end, 10);
+    program.duration   = strtoul(word_end, &word_end, 10);
     program.threads    = malloc(program.numThreads * sizeof(Thread));
 
     // Thread declaration
     char *token;
     char *state;
 
-    for (int i = 0; i < program.numThreads; i++) {
+    for (unsigned int i = 0; i < program.numThreads; i++) {
 
         Thread *thread = &program.threads[i];
 
@@ -218,24 +248,26 @@ ProgramInfo parseFile(char *filename) {
         // Thread type
         token = strtok_r(line, " ", &state);
         switch(token[0]) {
-            case 'P': thread->threadType = PERIODIC; break;
-            case 'A': thread->threadType = APERIODIC; break;
+            case 'P': thread->thread_type = PERIODIC; break;
+            case 'A': thread->thread_type = APERIODIC; break;
             default: break;
         }
 
         // Thread priority
         token = strtok_r(NULL, " ", &state);
-        thread->priority = strtol(token, NULL, 10);
+        thread->priority = (int) strtoul(token, NULL, 10);
 
-        // Aperiodic Trigger
-        if (thread->threadType == APERIODIC) {
+        // Aperiodic thread trigger
+        if (thread->thread_type == APERIODIC) {
             token = strtok_r(NULL, " ", &state);
-            thread->trigger = strtol(token, NULL, 10);
+            thread->event = strtoul(token, NULL, 10);  // 0 (LEFT) or 1 (RIGHT)
         }
-
-        // Thread period
-        token = strtok_r(NULL, " ", &state);
-        thread->period = strtol(token, NULL, 10);
+        // Periodic thread period
+        else {
+            // Thread period
+            token = strtok_r(NULL, " ", &state);
+            thread->period = strtoul(token, NULL, 10);
+        }
 
         // Thread operations
         Operation *root = NULL;
@@ -249,15 +281,15 @@ ProgramInfo parseFile(char *filename) {
             switch(token[0]) {
                 case 'L':
                     operation->operation = LOCK;
-                    operation->value = strtol(token + 1, NULL, 10);
+                    operation->value = strtoul(token + 1, NULL, 10);
                     break;
                 case 'U':
                     operation->operation = UNLOCK;
-                    operation->value = strtol(token + 1, NULL, 10);
+                    operation->value = strtoul(token + 1, NULL, 10);
                     break;
                 default:
                     operation->operation = BUSY_LOOP;
-                    operation->value = strtol(token, NULL, 10);
+                    operation->value = strtoul(token, NULL, 10);
                     break;
             }
 
@@ -293,31 +325,43 @@ int main(int argc, char* argv[]) {
 
     pthread_t threads[program.numThreads];  // TODO: Use later
     pthread_t mouse_watcher;
-    cpu_set_t cpuset;
 
+    pthread_barrier_init(&thread_sync, NULL, program.numThreads + 1); // Parent thread + created threads
+
+    cpu_set_t cpuset;
+    CPU_SET(1, &cpuset);
+
+    // Lock activation mutex
     pthread_mutex_lock(&activate_mut);
 
-    CPU_SET(1, &cpuset);
     pthread_create(&mouse_watcher, NULL, mouse_reader, "/dev/input/mice");
     pthread_setaffinity_np(mouse_watcher, sizeof(cpu_set_t), &cpuset);
+
     for (int i=0; i < program.numThreads; i++) {
-        switch (program.threads[i].threadType) {
-            case  PERIODIC:
-                {
-                    pthread_attr_t attr;
-                    struct sched_param param = {program.threads[i].priority};
-                    pthread_attr_init(&attr);
-                    pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
-                    pthread_attr_setschedparam(&attr, &param);
-                    pthread_create(&threads[i], &attr, periodic, &program.threads[i]);
-                    pthread_setaffinity_np(threads[i], sizeof(cpu_set_t), &cpuset);
-                }
-                break;
-            // case APERIODIC: aperiodic(&program.threads[i]); break;
-            default: break;
+
+        // Create attr for setting thread priority and policy
+        struct sched_param param;
+
+        // Thread attribute struct
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+
+        // Set thread priority, scheduling policy (FIFO), and affinity (1 CPU)
+        param.sched_priority = program.threads[i].priority;
+        pthread_attr_setschedparam(&attr, &param);
+        pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+        pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpuset);
+
+        // Start thread using attr struct
+        switch (program.threads[i].thread_type) {
+            case  PERIODIC: pthread_create(&threads[i], &attr,  periodic, &program.threads[i]); break;
+            case APERIODIC: pthread_create(&threads[i], &attr, aperiodic, &program.threads[i]); break;
         }
     }
 
+    pthread_barrier_wait(&thread_sync);      // Wait for children to be initialized and ready for broadcast
+    initialized = 1;
+    pthread_cond_broadcast(&activate_cond);  // Start all threads
     pthread_mutex_unlock(&activate_mut);
 
     struct timespec start_time;
