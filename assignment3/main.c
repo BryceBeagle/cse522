08073f282b70ed5d2ec56bd25f29d0c7af18a965
bkold <bakolden5@gmail.com>
2018-03-28 14:36:02 -0700

    test 2 almost done

--- assignment3/main.c
+++ assignment3/main.c
@@ -12,6 +12,8 @@
 #include <pinmux_galileo.h>
 #include <board.h>
 #include <misc/printk.h>
+#include "../drivers/pwm/pwm_pca9685.h"
+#include <limits.h>
 
 #define tsc_read() (_tsc_read())
 #define BUFFER_SIZE 500
@@ -24,15 +26,15 @@ enum pin_level {
 };
 
 typedef struct buffer_api {
-	u64_t *buffer;
-	unsigned int buffer_index;
-	struct k_mutex *buffer_lock;
+	u64_t * volatile buffer;
+	unsigned int volatile buffer_index;
 } buffer_api;
 
 u64_t interrupt_no_background_buffer[BUFFER_SIZE];
+u64_t interrupt_w_background_buffer_reference[BUFFER_SIZE];
 u64_t interrupt_w_background_buffer[BUFFER_SIZE];
 u64_t context_switch_buffer[BUFFER_SIZE];
-buffer_api current_buffer;
+buffer_api volatile current_buffer;
 K_THREAD_STACK_DEFINE(stack_area_0, STACKSIZE);
 K_THREAD_STACK_DEFINE(stack_area_1, STACKSIZE);
 
@@ -46,6 +48,8 @@ void sig_got(struct device *gpiob, struct gpio_callback *cb, u32_t pins);
 //test 2 variables
 K_SEM_DEFINE(threadA_sem, 1, 1);	/* starts off "available" */
 K_SEM_DEFINE(threadB_sem, 0, 1);	/* starts off "not available" */
+K_SEM_DEFINE(int_sem, 0, 500);	/* starts off "available" */
+K_SEM_DEFINE(done_mut, 1, 1);
 static struct gpio_callback gpio_cb;
 static struct k_thread threadA_data;
 static struct k_thread threadB_data;
@@ -53,6 +57,7 @@ void passer_thread(void *my_sem, void *other_sem, void *dummy3);
 
 //test 3 variables
 K_MUTEX_DEFINE(context_switch_mutex);
+K_MUTEX_DEFINE(interupt_mutex);
 static struct k_thread context_switch_higher_thread;
 static struct k_thread context_switch_lower_thread;
 void context_switch_higher(void *my_sem, void *other_sem, void *dummy3);
@@ -76,16 +81,17 @@ void ready_state_set(int value)
 void passer_thread(void *my_sem, void *other_sem, void *dummy3)
 {
 	ARG_UNUSED(dummy3);
-	// printk("%llu Starting", k_current_get());
 
 	/* invoke routine to ping-pong hello messages with threadA */
 	while (1) {
 		/* take my semaphore */
-		k_yield();
 		k_sem_take(my_sem, K_FOREVER);
-		// printk("My Turn %llu", k_current_get());
-		/* wait a while, then let other thread have a turn */
+		/* wait a while */
+		printk("Starting\n");
+		k_busy_wait(500);
+		/* let other thread have a turn */
 		k_sem_give(other_sem);
+		k_yield();
 	}
 }
 
@@ -120,11 +126,15 @@ void context_switch_lower(void *dummy1, void *dummy2, void *dummy3)
 
 void sig_got(struct device *gpiob, struct gpio_callback *cb, u32_t pins)
 {
-	*(current_buffer.buffer) = tsc_read() - *(current_buffer.buffer);
-	printk("SIG GOT\n");
-	current_buffer.buffer_index++;
-	current_buffer.buffer++;
-	ready_state_set(1);
+	u64_t now = tsc_read();
+	if(k_sem_count_get(&int_sem) < BUFFER_SIZE){
+		printk("Magic Print %d\n", k_sem_count_get(&int_sem));
+		current_buffer.buffer[current_buffer.buffer_index] = now - current_buffer.buffer[current_buffer.buffer_index];
+		current_buffer.buffer_index++;
+	}else{
+		k_sem_give(&done_mut);
+	}
+	k_sem_give(&int_sem);
 }
 
 void setup()
@@ -132,9 +142,9 @@ void setup()
 	k_mutex_init(ready_state_mutex);
 	ready_state_set(0);
 
-	struct device *EXP1 = device_get_binding(PINMUX_GALILEO_EXP1_NAME);
+	struct device *EXP1   = device_get_binding(PINMUX_GALILEO_EXP1_NAME);
 	struct device *GPIO_0 = device_get_binding(PINMUX_GALILEO_GPIO_DW_NAME);
-	struct device *PWM0 = device_get_binding(PINMUX_GALILEO_PWM0_NAME);
+	struct device *PWM0   = device_get_binding(PINMUX_GALILEO_PWM0_NAME);
 
 	//SHIELD PIN 3
 	if(pinmux_pin_set(device_get_binding(CONFIG_PINMUX_NAME), 3, PINMUX_FUNC_A)){
@@ -148,6 +158,9 @@ void setup()
 	if(pinmux_pin_set(device_get_binding(CONFIG_PINMUX_NAME), 9, PINMUX_FUNC_C)){
 		printk("ERROR 0.3\n");
 	}
+
+	pwm_pin_set_cycles(PWM0, 1, 0, 0);
+	pwm_pin_set_cycles(PWM0, 7, 0, 0);
 
 	//SHIELD PIN 0
 	// { EXP1,  0,   PIN_HIGH, (GPIO_DIR_OUT) }, /* GPIO3 int */
@@ -163,7 +176,6 @@ void setup()
 
 	gpio_init_callback(&gpio_cb, sig_got, BIT(3));
 	gpio_add_callback(GPIO_0, &gpio_cb);
-	gpio_pin_enable_callback(GPIO_0, 3);
 
 	if(gpio_pin_write(EXP1,    0, PIN_HIGH) ||
 	   gpio_pin_write(EXP1,    1, PIN_LOW)  )
@@ -171,111 +183,95 @@ void setup()
 		printk("ERROR 0.5\n");
 		return;
 	}
-
-
 }
 
 void main(void)
 {
 	printk("Testing Response Time!\n");
-	setup();
 	struct device *GPIO_0 = device_get_binding(PINMUX_GALILEO_GPIO_DW_NAME);
 	struct device *G_CW = device_get_binding(PINMUX_GALILEO_GPIO_INTEL_CW_NAME);
 	struct device *PWM0 = device_get_binding(PINMUX_GALILEO_PWM0_NAME);
 
-	printk("Running\n");
+	pwm_pin_set_cycles(PWM0, 7, 0, 0);
+	pwm_pin_set_cycles(PWM0, 1, 0, 0);
+	setup();
 
-	/*
-	 * I'm having trouble setting up PWM at the moment, everything else is done.
-	 */
-	// current_buffer.buffer_index = 0;
-	// current_buffer.buffer = interrupt_w_background_buffer;
+	{
+		u64_t time1 = tsc_read();
+		printk("Running %llu\n", time1);
+		u64_t time2 = tsc_read();
+		printk("Time to print = %llu\n", time2 - time1);
+	}
 
-	// {
-		// printk("Setting-1 : %d\n", pwm_pin_set_cycles(PWM0,  7, 100, 4096));
-	// 	k_sleep(2000);
-		// printk("Setting0 : %d\n", pwm_pin_set_cycles(PWM0,  7, 100, 1000));
-		// k_sleep(2000);
-		// printk("Setting1 : %d\n", pwm_pin_set_cycles(PWM0,  7, 100, 1500));
-		// k_sleep(2000);
-		// printk("Setting2 : %d\n", pwm_pin_set_cycles(PWM0,  7, 100, 2000));
-		// k_sleep(2000);
-		// printk("Setting3 : %d\n", pwm_pin_set_cycles(PWM0,  7, 100, 2500));
-		// k_sleep(2000);
-		// printk("Setting4 : %d\n", pwm_pin_set_cycles(PWM0,  7, 100, 3000));
-		// k_sleep(2000);
-		// printk("Setting5 : %d\n", pwm_pin_set_cycles(PWM0,  7, 100, 3500));
-		// k_sleep(2000);
-		// printk("Setting6 : %d\n", pwm_pin_set_cycles(PWM0,  7, 100, 4000));
-		// k_sleep(2000);
-		printk("Setting7 : %d\n", pwm_pin_set_cycles(PWM0,  7, 100, 4500));
-		k_sleep(2000);
-		// printk("Setting8 : %d\n", pwm_pin_set_cycles(PWM0,  7, 100, 5000));
-		// k_sleep(2000);
-		// printk("Setting4 : %d\n", pwm_pin_set_cycles(PWM0,  7, 100, 3000));
-		// k_sleep(2000);
-	// }
+	pwm_pin_set_cycles(PWM0, 7, 0, 100);
+	pwm_pin_set_cycles(PWM0, 1, 0, 100);
 
 	// no background
 	printk("Running Test 1\n");
 	{
 		current_buffer.buffer_index = 0;
 		current_buffer.buffer = interrupt_no_background_buffer;
-		while(current_buffer.buffer_index < BUFFER_SIZE)
+		k_sem_reset(&int_sem);
+		gpio_pin_enable_callback(GPIO_0, 3);
+		while(k_sem_count_get(&int_sem) < BUFFER_SIZE)
 		{
-
-			// k_sleep(50);
-			*(current_buffer.buffer) = tsc_read();
+			current_buffer.buffer[current_buffer.buffer_index] = tsc_read();
 			gpio_pin_write(GPIO_0, 6, 1);
-			gpio_pin_write(GPIO_0, 6, 0);
 			gpio_pin_write(G_CW,   0, 1);
-			gpio_pin_write(G_CW, 0, 0);
+			k_sleep(20);
 
-			int ready;
-			do{
-				ready_state_take(&ready);
-			}while(ready == 0);
-
-			// k_sleep(50);
-
+			gpio_pin_write(GPIO_0, 6, 0);
+			gpio_pin_write(G_CW,   0, 0);
+			k_sleep(20);
 		}
 	}
 
 	//with background
 	printk("Running Test 2\n");
 	{
-		current_buffer.buffer_index = 0;
-		current_buffer.buffer = interrupt_w_background_buffer;
+		pinmux_pin_set(device_get_binding(CONFIG_PINMUX_NAME), 3, PINMUX_FUNC_C);
+
+		for (int i = 0; i < BUFFER_SIZE; i++)
+		{
+			interrupt_w_background_buffer_reference[i] = 0;
+			interrupt_w_background_buffer[i] = 0;
+		}
+
+		// get with background data
+
 		k_tid_t ta = k_thread_create(&threadA_data, stack_area_0, STACKSIZE,
 				passer_thread, &threadA_sem, &threadB_sem, NULL,
-				k_thread_priority_get(k_current_get()), 0, K_NO_WAIT);
+				k_thread_priority_get(k_current_get())+1, 0, K_NO_WAIT);
 		k_tid_t tb = k_thread_create(&threadB_data, stack_area_1, STACKSIZE,
 				passer_thread, &threadB_sem, &threadA_sem, NULL,
-				k_thread_priority_get(k_current_get()), 0, K_NO_WAIT);
-		while(current_buffer.buffer_index < BUFFER_SIZE)
+				k_thread_priority_get(k_current_get())+1, 0, K_NO_WAIT);
+
+		current_buffer.buffer_index = 0;
+		current_buffer.buffer = interrupt_w_background_buffer;
+		k_sem_reset(&int_sem);
+		k_busy_wait(500);
+
+		k_sem_take(&done_mut, K_FOREVER);
+		while(k_sem_take(&done_mut, K_NO_WAIT) != 0)
 		{
-			k_sleep(50);
-
-			*(current_buffer.buffer) = tsc_read();
-
-
-			// gpio_pin_write(GPIO_0, 6, 1);
-			// gpio_pin_write(G_CW,   0, 1);
-
-			int ready;
-			do{
-				ready_state_take(&ready);
-				k_sleep(1020);
-				printk("retry\n");
-			}while(ready == 0);
-
-			k_sleep(50);
-
-			// gpio_pin_write(GPIO_0, 6, 0);
-			// gpio_pin_write(G_CW,   0, 0);
+			k_sleep(500);
 		}
+
 		k_thread_abort(ta);
 		k_thread_abort(tb);
+
+		current_buffer.buffer_index = 0;
+		current_buffer.buffer = interrupt_w_background_buffer_reference;
+		k_sem_reset(&int_sem);
+		k_busy_wait(500);
+
+		//get reference data
+		k_sem_take(&done_mut, K_FOREVER);
+		while(k_sem_take(&done_mut, K_NO_WAIT) != 0)
+		{
+			k_sleep(500);
+		}
+		gpio_pin_disable_callback(device_get_binding(PINMUX_GALILEO_GPIO_DW_NAME), 3);
 	}
 
 	//context switch
@@ -285,9 +281,9 @@ void main(void)
 		current_buffer.buffer_index = 0;
 		current_buffer.buffer = context_switch_buffer;
 		k_tid_t t1 = k_thread_create(&context_switch_higher_thread, stack_area_0, STACKSIZE,
-				context_switch_higher, NULL, NULL, NULL, 5, 0, 200);
+				context_switch_higher, NULL, NULL, NULL, k_thread_priority_get(k_current_get())+1, 0, 200);
 		k_tid_t t2 = k_thread_create(&context_switch_lower_thread, stack_area_1, STACKSIZE,
-				context_switch_lower, NULL, NULL, NULL, 10, 0, 200);
+				context_switch_lower, NULL, NULL, NULL, k_thread_priority_get(k_current_get())+5, 0, 200);
 		while(current_buffer.buffer_index < BUFFER_SIZE)
 		{
 			k_sleep(500);
@@ -299,8 +295,11 @@ void main(void)
 	//results
 	{
 		int average_tcl_int_no_background = 0;
+		int average_tcl_int_w_background_reference = 0;
 		int average_tcl_int_w_background = 0;
 		int average_tcl_int_context_switch = 0;
+
+		//TEST 1///////////////////////////////////
 		current_buffer.buffer = interrupt_no_background_buffer;
 		for (int i = 0; i < BUFFER_SIZE; i++)
 		{
@@ -308,13 +307,37 @@ void main(void)
 		}
 		average_tcl_int_no_background /= 500;
 
-		current_buffer.buffer = interrupt_w_background_buffer;
-		for (int i = 0; i < BUFFER_SIZE; i++)
+		//TEST 2///////////////////////////////////
 		{
-			average_tcl_int_w_background += current_buffer.buffer[i];
-		}
-		average_tcl_int_w_background /= 500;
+			int pwm_period_reference = 0;
+			int pwm_start_reference = 0;
 
+			current_buffer.buffer = interrupt_w_background_buffer_reference;
+			for (int i = 1; i < BUFFER_SIZE; i++)
+			{
+				pwm_period_reference += current_buffer.buffer[i] - current_buffer.buffer[i-1];
+			}
+			pwm_period_reference /= 499;
+			pwm_start_reference = (current_buffer.buffer[0] % pwm_period_reference) - average_tcl_int_no_background;
+			// printk("period : %d\n", pwm_period_reference);
+			// printk("start time %d\n", pwm_start_reference);
+			for (int i = 0; i < BUFFER_SIZE; i++)
+			{
+				average_tcl_int_w_background_reference += ((current_buffer.buffer[i] % pwm_period_reference) - pwm_start_reference) % pwm_period_reference;
+			}
+			average_tcl_int_w_background_reference /= 500;
+
+			current_buffer.buffer = interrupt_w_background_buffer;
+			for (int i = 0; i < BUFFER_SIZE; i++)
+			{
+				average_tcl_int_w_background += ((current_buffer.buffer[i] % pwm_period_reference) - pwm_start_reference) % pwm_period_reference;
+			}
+			average_tcl_int_w_background /= 500;
+			// printk("Test 2.0: \t-> %d\n", average_tcl_int_w_background_reference);
+			// printk("Test 2.1: \t-> %d\n", average_tcl_int_w_background);
+		}
+
+		//TEST 3///////////////////////////////////
 		current_buffer.buffer = context_switch_buffer;
 		for (int i = 0; i < BUFFER_SIZE; i++)
 		{
@@ -322,8 +345,8 @@ void main(void)
 		}
 		average_tcl_int_context_switch /= 500;
 
-		printk("Test 1 : Interrupt with no background -> %d\n", average_tcl_int_no_background);
-		printk("Test 2 : Interrupt with background -> %d\n", average_tcl_int_w_background);
-		printk("Test 3 : context switch with no background -> %d\n", average_tcl_int_context_switch);
+		printk("Test 1  : Interrupt with no background \t\t-> %d\n", average_tcl_int_no_background);
+		printk("Test 2  : Interrupt with background \t\t-> %d\n", average_tcl_int_w_background - average_tcl_int_w_background_reference);
+		printk("Test 3  : context switch with no background \t-> %d\n", average_tcl_int_context_switch);
 	}
 }
