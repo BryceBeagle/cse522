3f64c68af9ea64815dda98619e4bc31908f8e675
brycebeagle <bryce.beagle@gmail.com>
2018-03-31 00:08:50 -0700

    Added averages

--- assignment3/histogram.py
+++ assignment3/histogram.py
@@ -6,26 +6,29 @@ output2 = "2.out"
 output3 = "3.out"
 
 res = np.genfromtxt(output1, delimiter="\n")
+print(f"Average 1: {res.mean()}")
 
 plot.title(f"Interrupt Latency Without Background Computing")
 plot.xlabel("Cycles")
 plot.ylabel("Percent of results")
-plot.hist(res, normed=True, bins=15)
+plot.hist(res, bins=15)
 plot.show()
 
 res = np.genfromtxt(output2, delimiter="\n")
+print(f"Average 2: {res.mean()}")
 
 plot.title(f"Interrupt Latency With Background Computing")
 plot.xlabel("Cycles")
 plot.ylabel("Percent of results")
-plot.hist(res, normed=True, bins=15)
+plot.hist(res, bins=15)
 plot.show()
 
 res = np.genfromtxt(output3, delimiter="\n")
+print(f"Average 3: {res.mean()}")
 
 plot.title(f"Context Switch Overhead")
 plot.xlabel("Cycles")
 plot.ylabel("Percent of results")
-plot.hist(res, normed=True, bins=15)
+plot.hist(res, bins=15)
 plot.show()
 
