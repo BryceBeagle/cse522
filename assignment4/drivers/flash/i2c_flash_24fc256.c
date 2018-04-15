3827f44a1d7e32dc0819ffb20377aeb1307fef2e
bkold <bakolden5@gmail.com>
2018-04-14 17:04:58 -0700

    updated eeprom driver

--- assignment4/drivers/flash/i2c_flash_24fc256.c
+++ assignment4/drivers/flash/i2c_flash_24fc256.c
@@ -14,17 +14,20 @@
 //view i2c_flash_w25qxxdv.c for reference
 //must implement checks and retrys for when device is busy
 
+#define ADDRESS_COUNT (32*1024)
+#define PAGE_COUNT (ADDRESS_COUNT/64)
+
 static int i2c_flash_wb_read(struct device *dev, off_t offset, void *data,
 			     size_t len)
 {
 	// random sequential read
+	// offset is relative to pages rather than bytes
 	u8_t read_control_buffer[2];
 	int ret = 0;
 	k_sem_take(&driver_data->sem, K_FOREVER);
 
 	read_control_buffer[0] = (offset & 0xFF00) >> 8;
-	read_control_buffer[1] = (offset & 0x00FF) >> 0;
-	read_data[0] = 0xA1;
+	read_control_buffer[1] = (offset & 0x00C0) >> 0;
 
 	ret |= i2c_write(driver_data->i2c, read_control_buffer, 2, 0xA0);
 	ret |= i2c_read(driver_data->i2c, data, len, 0xA1);
@@ -37,19 +40,19 @@ static int i2c_flash_wb_write(struct device *dev, off_t offset,
 			      const void *data, size_t len)
 {
 	// only allow page writes (64 bytes)
-	u8_t buffer[67];
+	// offset is relative to pages rather than bytes
+	u8_t buffer[66];
 	int ret = 0;
 	k_sem_take(&driver_data->sem, K_FOREVER);
 
-	if(len != 64){
+	if(len > 64){
 		ret = -1;
 	}else{
-		buffer[0] = 0xA0;
-		buffer[1] = (offset & 0xFF00) >> 8;
-		buffer[2] = (offset & 0x00FF) >> 0;
-		memcpy(buffer+3, data, len);
+		buffer[0] = (offset & 0xFF00) >> 8;
+		buffer[1] = (offset & 0x00C0) >> 0;
+		memcpy(buffer+2, data, len);
 
-		ret |= i2c_write(driver_data->i2c, buffer, 0xA0);
+		ret = i2c_write(driver_data->i2c, buffer, len, 0xA0);
 	}
 
 	k_sem_give(&driver_data->sem);
@@ -69,6 +72,14 @@ static int i2c_flash_wb_erase(struct device *dev, off_t offset, size_t size)
 {
 	// call write with an array of zeros for every page
 	k_sem_take(&driver_data->sem, K_FOREVER);
+
+	u8_t zeros[64];
+	memset(zeros, 0, sizeof(zeros));
+
+	for(int i = 0; i < size; i++)
+	{
+		i2c_flash_wb_write(dev, i, zeros, sizeof(zeros));
+	}
 
 	k_sem_give(&driver_data->sem);
 
