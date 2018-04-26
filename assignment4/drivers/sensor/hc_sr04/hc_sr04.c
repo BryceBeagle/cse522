70eb7c8bd08abd34206007fad45355ce6c6fec58
brycebeagle <bryce.beagle@gmail.com>
2018-04-26 02:22:50 -0700

    Commented out the input stuff and included changes to distance sensor
    - Includes debug printout
    - Distance sensor now uses val1 and val2

--- assignment4/drivers/sensor/hc_sr04/hc_sr04.c
+++ assignment4/drivers/sensor/hc_sr04/hc_sr04.c
@@ -22,45 +22,60 @@ enum pin_level {
 #define tsc_read() (_tsc_read())
 
 static void hc_sr04_gpio_callback(struct device *dev,
-				  struct gpio_callback *cb, u32_t pins)
-{
+                                  struct gpio_callback *cb, u32_t pins) {
+
+	printk("    Callback called\n");
+
+	printk("    CB Reading TSC\n");
 	u64_t now = tsc_read();
 	struct hc_sr04_data *drv_data =
-		CONTAINER_OF(cb, struct hc_sr04_data, gpio_cb);
+			CONTAINER_OF(cb, struct hc_sr04_data, gpio_cb);
 
 	ARG_UNUSED(pins);
 
 	drv_data->distance = (now - drv_data->distance) * 1000000 / 400000000 / 58;
 
+	printk("    CB Giving semaphore\n");
 	k_sem_give(&drv_data->data_sem);
 }
 
-static int hc_sr04_sample_fetch(struct device *dev, enum sensor_channel chan)
-{
+static int hc_sr04_sample_fetch(struct device *dev, enum sensor_channel chan) {
 	struct hc_sr04_data *drv_data = dev->driver_data;
 
+	printk("    Taking semaphore\n");
 	k_sem_take(&drv_data->data_sem, K_FOREVER);
 
+	printk("    Reading TSC\n");
 	drv_data->distance = tsc_read();
+
+	printk("    Setting pin high\n");
 	gpio_pin_write(drv_data->gpio, CONFIG_HC_SR04_GPIO_PIN_NUM_2, PIN_HIGH);
+
+	printk("    Waiting for 10 us\n");
 	k_busy_wait(10);
+
+	printk("    Setting pin low\n");
 	gpio_pin_write(drv_data->gpio, CONFIG_HC_SR04_GPIO_PIN_NUM_2, PIN_LOW);
 
+	printk("    Taking semaphore again\n");
 	k_sem_take(&drv_data->data_sem, K_FOREVER);
 
+	printk("    Giving semaphore");
+	k_sem_give(&drv_data->data_sem);
+
+	printk("Returning\n");
 	return 0;
 }
 
 
 static int hc_sr04_channel_get(struct device *dev,
-			       enum sensor_channel chan,
-			       struct sensor_value *val)
-{
+                               enum sensor_channel chan,
+                               struct sensor_value *val) {
 	struct hc_sr04_data *drv_data = dev->driver_data;
 
 	if (chan == SENSOR_CHAN_PROX) {
-		val->val1 = drv_data->distance;
-		val->val2 = 0;
+		val->val1 = (s32_t) drv_data->distance;
+		val->val2 = (s32_t) ((drv_data->distance - val->val1) * 1E6);
 	} else {
 		return -ENOTSUP;
 	}
@@ -69,59 +84,70 @@ static int hc_sr04_channel_get(struct device *dev,
 }
 
 static const struct sensor_driver_api hc_sr04_driver_api = {
-	.sample_fetch = hc_sr04_sample_fetch,
-	.channel_get = hc_sr04_channel_get,
+		.sample_fetch = hc_sr04_sample_fetch,
+		.channel_get  = hc_sr04_channel_get,
 };
 
-static int hc_sr04_init(struct device *dev)
-{
+static int hc_sr04_init(struct device *dev) {
 	struct hc_sr04_data *drv_data = dev->driver_data;
 
-	drv_data->exp  = device_get_binding(CONFIG_HC_SR04_EXP_DEV_NAME);
+	drv_data->exp = device_get_binding(CONFIG_HC_SR04_EXP_DEV_NAME);
 	drv_data->gpio = device_get_binding(CONFIG_HC_SR04_GPIO_DEV_NAME);
 	if (drv_data->exp == NULL) {
-		SYS_LOG_DBG("Failed to get pointer to %s device",
-			    CONFIG_HC_SR04_EXP_DEV_NAME);
+		SYS_LOG_DBG("Failed to get pointer to %s device", CONFIG_HC_SR04_EXP_DEV_NAME);
 		return -EINVAL;
 	}
 	if (drv_data->gpio == NULL) {
-		SYS_LOG_DBG("Failed to get pointer to %s device",
-			    CONFIG_HC_SR04_GPIO_DEV_NAME);
+		SYS_LOG_DBG("Failed to get pointer to %s device", CONFIG_HC_SR04_GPIO_DEV_NAME);
 		return -EINVAL;
 	}
 
-	k_sem_init(&drv_data->data_sem, 0, 1);
+	k_sem_init(&drv_data->data_sem, 1, 1);
+
+	int res;
 
 	/* setup data ready gpio trigger on shield pin 2*/
-	gpio_pin_configure(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_3, GPIO_DIR_OUT);
-	gpio_pin_write(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_3, PIN_LOW);
-	gpio_pin_configure(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_4, GPIO_DIR_OUT);
-	gpio_pin_write(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_4, PIN_LOW);
-	gpio_pin_configure(drv_data->gpio, CONFIG_HC_SR04_GPIO_PIN_NUM_2, GPIO_DIR_OUT);
-	gpio_pin_write(drv_data->gpio, CONFIG_HC_SR04_GPIO_PIN_NUM_2, PIN_LOW);
+	res = gpio_pin_configure(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_3, GPIO_DIR_OUT);
+	if (res) printk("Error 1\n");
+	res = gpio_pin_configure(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_4, GPIO_DIR_OUT);
+	if (res) printk("Error 2\n");
+	res = gpio_pin_configure(drv_data->gpio, CONFIG_HC_SR04_GPIO_PIN_NUM_2, GPIO_DIR_OUT);
+	if (res) printk("Error 3\n");
+
+	res = gpio_pin_write(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_3, PIN_LOW);
+	if (res) printk("Error 4\n");
+	res = gpio_pin_write(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_4, PIN_LOW);
+	if (res) printk("Error 5\n");
+	res = gpio_pin_write(drv_data->gpio, CONFIG_HC_SR04_GPIO_PIN_NUM_2, PIN_LOW);
+	if (res) printk("Error 6\n");
 
 	/* setup data ready gpio interrupt on shield pin 0*/
-	gpio_pin_configure(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_1, GPIO_DIR_OUT);
-	gpio_pin_write(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_1, PIN_HIGH);
-	gpio_pin_configure(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_2, GPIO_DIR_OUT);
-	gpio_pin_write(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_2, PIN_LOW);
+	res = gpio_pin_configure(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_1, GPIO_DIR_OUT);
+	if (res) printk("Error 7\n");
+	res = gpio_pin_configure(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_2, GPIO_DIR_OUT);
+	if (res) printk("Error 8\n");
+	res = gpio_pin_write(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_1, PIN_HIGH);
+	if (res) printk("Error 9\n");
+	res = gpio_pin_write(drv_data->exp, CONFIG_HC_SR04_EXP_PIN_NUM_2, PIN_LOW);
+	if (res) printk("Error 10\n");
 
-	gpio_pin_configure(drv_data->gpio, CONFIG_HC_SR04_GPIO_PIN_NUM_1,
-			   GPIO_DIR_IN | GPIO_INT | GPIO_INT_EDGE | GPIO_INT_ACTIVE_HIGH);
-	gpio_init_callback(&drv_data->gpio_cb,
-			   hc_sr04_gpio_callback,
-			   BIT(CONFIG_HC_SR04_GPIO_PIN_NUM_1));
+	res = gpio_pin_configure(drv_data->gpio, CONFIG_HC_SR04_GPIO_PIN_NUM_1,
+	                         GPIO_DIR_IN | GPIO_INT | GPIO_INT_EDGE | GPIO_INT_ACTIVE_HIGH);
+	if (res) printk("Error 11\n");
+	gpio_init_callback(&drv_data->gpio_cb, hc_sr04_gpio_callback,
+	                   BIT(CONFIG_HC_SR04_GPIO_PIN_NUM_1));
 	if (gpio_add_callback(drv_data->gpio, &drv_data->gpio_cb) < 0) {
 		SYS_LOG_DBG("Failed to set GPIO callback");
 		return -EIO;
 	}
+	gpio_pin_enable_callback(drv_data->gpio, CONFIG_HC_SR04_GPIO_PIN_NUM_1);
 
 	dev->driver_api = &hc_sr04_driver_api;
 
 	return 0;
 }
-
+`
 static struct hc_sr04_data hc_sr04_data;
 
 DEVICE_INIT(hc_sr04, CONFIG_HC_SR04_NAME, hc_sr04_init, &hc_sr04_data,
-	    NULL, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY);
+            NULL, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY);
--- assignment4/samples/assignment4/src/main.c
+++ assignment4/samples/assignment4/src/main.c
@@ -11,6 +11,8 @@
 #include <device.h>
 #include <sensor.h>
 #include <console.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 #define STACKSIZE 1024
@@ -18,104 +20,116 @@
 K_THREAD_STACK_DEFINE(stack_area_0, STACKSIZE);
 K_THREAD_STACK_DEFINE(stack_area_1, STACKSIZE);
 
-void radar_read(void *hc_device, void *b, void *c)
-{
+void radar_read(void *hc_device, void *b, void *c) {
+	printk("Thread running");
+
 	ARG_UNUSED(b);
 	ARG_UNUSED(c);
 
 	struct device *HCSR = hc_device;
-	while(1)
-	{
-		NULL;
+
+	while (1) {
+
+		struct sensor_value distance;
+
+		printk("Fetching\n");
+		sensor_sample_fetch(HCSR);
+
+		printk("Getting distance\n");
+		sensor_channel_get(HCSR, SENSOR_CHAN_PROX, &distance);
+
+		printk("Distance: %d\n", distance.val1);
+
+		k_sleep(1000);
+
 	}
 }
 
-void main(void)
-{
+void main(void) {
 
-	struct k_thread hcsr_thread_0;
-	struct k_thread hcsr_thread_1;
-	k_tid_t hcsr_thread_0_tid;
-	k_tid_t hcsr_thread_1_tid;
+	printk("Main starting\n");
 
-	struct device *EEPROM_0;
-	struct device *HCSR_0;
-	struct device *HCSR_1;
+	struct k_thread hcsr_thread_0, hcsr_thread_1;
+	k_tid_t hcsr_thread_0_tid, hcsr_thread_1_tid;
 
+	struct device *EEPROM_0 = device_get_binding(CONFIG_I2C_FLASH_24FC256_DRV_NAME);
+	struct device *HCSR_0 = device_get_binding(CONFIG_HC_SR04_NAME);
+//	struct device *HCSR_1 = device_get_binding(CONFIG_HC_SR04_NAME);
+
+	printk("Creating thread\n");
 	hcsr_thread_0_tid = k_thread_create(&hcsr_thread_0, stack_area_0, STACKSIZE,
 	                                    radar_read, HCSR_0, NULL, NULL,
-	                                    k_thread_priority_get(k_current_get())+1, 0, K_NO_WAIT);
+	                                    k_thread_priority_get(k_current_get()) + 1, 0, K_NO_WAIT);
 
-	hcsr_thread_1_tid = k_thread_create(&hcsr_thread_1, stack_area_1, STACKSIZE,
-	                                    radar_read, HCSR_1, NULL, NULL,
-	                                    k_thread_priority_get(k_current_get())+1, 0, K_NO_WAIT);
+	printk("Thread created\n");
 
-	//perhaps set time slicing for radar tasks
-
-	k_thread_suspend(hcsr_thread_1_tid);
-	k_thread_suspend(hcsr_thread_0_tid);
-
-	console_getline_init();
-	while(1)
-	{
-		char *command_line = console_getline();
-		char *command_type = strtok(command_line, " ");
-		if(strcmp(command_type, "Enable")){
-			//enable the two threads running the sensors
-			char *input_parameter = strtok(NULL, " ");
-			char n = strtol(p_string, NULL, 10);
-
-			switch(n){
-				case 2:
-					//enable HCSR_0's thread
-					//enable HCSR_1's thread
-					k_thread_resume(hcsr_thread_0_tid);
-					k_thread_resume(hcsr_thread_1_tid);
-					break;
-				case 1:
-					//enable HCSR_0's thread
-					//disable HCSR_1's thread
-					k_thread_resume(hcsr_thread_0_tid);
-					k_thread_suspend(hcsr_thread_1_tid);
-					break;
-				case 0:
-					//disable HCSR_0's thread
-					//disable HCSR_1's thread
-					k_thread_suspend(hcsr_thread_0_tid);
-					k_thread_suspend(hcsr_thread_1_tid);
-					break;
-				default:
-					printk("Exiting\n");
-					exit(-1);
-			}
-
-		}else if(strcmp(command_type, "Start")){
-			//erase the pages up to input p
-			char *p_string = strtok(NULL, " ");
-			size_t p = strtol(p_string, NULL, 10);
-
-			flash_erase(EEPROM_0, NULL, p);
-
-		}else if(strcmp(command_type, "Dump")){
-			//print pages between input p1 and p2
-			char *p1_string = strtok(NULL, " ");
-			off_t p1 = strtol(p1_string, NULL, 10);
-			char *p2_string = strtok(NULL, " ");
-			off_t p2 = strtol(p2_string, NULL, 10);
-			int page_buffer[1024];
-
-			for(off_t i = p1; i <= p2; i++)
-			{
-				flash_read(EEPROM_0, p1, page_buffer, sizeof(page_buffer));
-				for(int x = 0; x < 1024; x++)
-				{
-					printk("%d\n", page_buffer[x]);
-				}
-			}
-
-		}
-
-		//yield to the threads in some fashion
-		NULL;
-	}
+//	hcsr_thread_1_tid = k_thread_create(&hcsr_thread_1, stack_area_1, STACKSIZE,
+//	                                    radar_read, HCSR_1, NULL, NULL,
+//	                                    k_thread_priority_get(k_current_get()) + 1, 0, K_NO_WAIT);
+//
+//	//perhaps set time slicing for radar tasks
+//
+//	k_thread_suspend(hcsr_thread_0_tid);
+//	k_thread_suspend(hcsr_thread_1_tid);
+//
+//	console_getline_init();
+//	while (1) {
+//		char *command_line = console_getline();
+//		char *command_type = strtok(command_line, " ");
+//		if (strcmp(command_type, "Enable")) {
+//			//enable the two threads running the sensors
+//			char *input_parameter = strtok(NULL, " ");
+//			char n = strtol(p_string, NULL, 10);
+//
+//			switch (n) {
+//				case 2:
+//					//enable HCSR_0's thread
+//					//enable HCSR_1's thread
+//					k_thread_resume(hcsr_thread_0_tid);
+//					k_thread_resume(hcsr_thread_1_tid);
+//					break;
+//				case 1:
+//					//enable HCSR_0's thread
+//					//disable HCSR_1's thread
+//					k_thread_resume(hcsr_thread_0_tid);
+//					k_thread_suspend(hcsr_thread_1_tid);
+//					break;
+//				case 0:
+//					//disable HCSR_0's thread
+//					//disable HCSR_1's thread
+//					k_thread_suspend(hcsr_thread_0_tid);
+//					k_thread_suspend(hcsr_thread_1_tid);
+//					break;
+//				default:
+//					printk("Exiting\n");
+//					exit(-1);
+//			}
+//
+//		} else if (strcmp(command_type, "Start")) {
+//			//erase the pages up to input p
+//			char *p_string = strtok(NULL, " ");
+//			size_t p = strtol(p_string, NULL, 10);
+//
+//			flash_erase(EEPROM_0, NULL, p);
+//
+//		} else if (strcmp(command_type, "Dump")) {
+//			//print pages between input p1 and p2
+//			char *p1_string = strtok(NULL, " ");
+//			off_t p1 = strtol(p1_string, NULL, 10);
+//			char *p2_string = strtok(NULL, " ");
+//			off_t p2 = strtol(p2_string, NULL, 10);
+//			int page_buffer[1024];
+//
+//			for (off_t i = p1; i <= p2; i++) {
+//				flash_read(EEPROM_0, p1, page_buffer, sizeof(page_buffer));
+//				for (int x = 0; x < 1024; x++) {
+//					printk("%d\n", page_buffer[x]);
+//				}
+//			}
+//
+//		}
+//
+//		//yield to the threads in some fashion
+//		NULL;
+//	}
 }
